28319f862c18f96df259125848d215e2c433e64b
Nikola Cuculovski <n.cuculovski@gmail.com>
2022-11-05 13:34:39 -0400

    Add files via upload

--- home.py
+++ home.py
@@ -682,11 +682,11 @@ if check_password():
         st.markdown(
             """
         
-        Fabrizio Freda, President, Chief Executive Officer & Director, The Estée Lauder Companies, Inc., acknowledges that the *“prolonged pandemic, the invasion of Ukraine, and the onset of higher inflation”* are external factors and considerations affecting business performance.
+        *Fabrizio Freda, President, Chief Executive Officer & Director, The Estée Lauder Companies, Inc.*, acknowledges that the *“prolonged pandemic, the invasion of Ukraine, and the onset of higher inflation”* are external factors and considerations affecting business performance.
         
         According to Bloomberg, and a report conducted by IRI, a data analytics and market research company, it *“revealed that 58% of consumers already report that they have cut down on essentials, with 35% dipping into their personal savings and taking out loans to pay bills.”*
         
-        Regardless of the macroeconomic situation, the category and its main players including Estee Lauder are starting to have visible downshift in stock performance. To battle such forces, we need additional optimization levers that will enable budget fluidity and flexibility.
+        Regardless of the macroeconomic situation, the category and its main players including Estée Lauder are starting to have visible downshift in stock performance. To battle such forces, we need additional optimization levers that will enable budget fluidity and flexibility.
 
         """
         )
